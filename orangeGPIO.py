7fb3f13da6dd466174a629d853927c34f423d5d2
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-07-26 15:39:37 -0230

    Update orangeGPIO.py

--- orangeGPIO.py
+++ orangeGPIO.py
@@ -2,8 +2,7 @@ import OPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(6, GPIO.OUT)
-print("Led on")
 GPIO.output(6,GPIO.High)
 time.sleep(1)
-print("LED off")
 GPIO.output(6,GPIO.LOW)
+GPIO.cleanup()
