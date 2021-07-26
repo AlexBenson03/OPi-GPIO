637e99b47591a6b6ba04daf6909a91db26fc57d4
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-07-26 16:58:26 -0230

    Create buttoninput.py

---
+++ buttoninput.py
@@ -0,0 +1,7 @@
+import OPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.add_event_detect(12, GPIO.Rising)
+if GPIO.event_detected(12):
+    print("the button was pressed")
+GPIO.cleanup()
