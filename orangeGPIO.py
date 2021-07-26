f1e7f0871b8a08854248ae24a0759cb8096e15e0
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-07-26 15:52:39 -0230

    Update orangeGPIO.py

--- orangeGPIO.py
+++ orangeGPIO.py
@@ -1,8 +1,8 @@
 import OPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(6, GPIO.OUT)
-GPIO.output(6,GPIO.High)
+GPIO.setup(7, GPIO.OUT)
+GPIO.output(7,GPIO.High)
 time.sleep(1)
-GPIO.output(6,GPIO.LOW)
+GPIO.output(7,GPIO.LOW)
 GPIO.cleanup()
