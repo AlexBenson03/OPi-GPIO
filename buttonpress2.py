c53d2622642f25979279a017af11a6043dec6b54
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-07-27 15:50:29 -0230

    Update buttonpress2.py

--- buttonpress2.py
+++ buttonpress2.py
@@ -3,11 +3,13 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.IN)
 GPIO.setup(7,GPIO.OUT)
-while GPIO.input(12) == GPIO.LOW:
-    time.sleep(0.01)
-GPIO.output(7, GPIO.HIGH)
-time.sleep(1)
-GPIO.output(7, GPIO.LOW)
-print("pressed")
+i=2
+n=0
+while i > n:
+    while GPIO.input(12) == GPIO.LOW:
+        time.sleep(0.01)
+    GPIO.output(7, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(7, GPIO.LOW)
+    print("pressed")
 GPIO.cleanup()
-
