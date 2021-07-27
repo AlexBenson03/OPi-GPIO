94afa039bec973e782abef19f2324e1d9f80300e
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-07-27 15:56:06 -0230

    changed the While loop

--- buttonpress2.py
+++ buttonpress2.py
@@ -1,15 +1,15 @@
 import OPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.IN)
-GPIO.setup(7,GPIO.OUT)
 i=2
 n=0
 while i > n:
+    GPIO.setup(12, GPIO.IN)
+    GPIO.setup(7,GPIO.OUT)
     while GPIO.input(12) == GPIO.LOW:
         time.sleep(0.01)
     GPIO.output(7, GPIO.HIGH)
     time.sleep(1)
     GPIO.output(7, GPIO.LOW)
     print("pressed")
-GPIO.cleanup()
+    GPIO.cleanup()
