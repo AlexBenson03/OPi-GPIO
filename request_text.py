65fad70f9ad6761483cac9ebe77b6e8524ed7f8f
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-04 14:38:30 -0230

    Update request_text.py

--- request_text.py
+++ request_text.py
@@ -3,7 +3,7 @@ import OPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.IN)
-api_url = 'http://8.16.250.212:4000/api/job'
+api_url = 'http://8.16.250.212:4000/api/printer'
 myheaders = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json' }
 i = 2
 n = 1
