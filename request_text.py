22cb26171cf2df154d5a5712f8d1ccf5b6a5b93d
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-04 14:37:54 -0230

    Create request_text.py

---
+++ request_text.py
@@ -0,0 +1,17 @@
+import requests
+import OPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.IN)
+api_url = 'http://8.16.250.212:4000/api/job'
+myheaders = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json' }
+i = 2
+n = 1
+while i > n:
+    while GPIO.input(12) == GPIO.LOW:
+        time.sleep(0.01)
+    if GPIO.input(12) == GPIO.HIGH:
+        response = requests.get(api_url, headers = myheaders)
+        print(response.json())
+        print(response.status_code)
+    
