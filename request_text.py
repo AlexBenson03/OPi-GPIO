cd324de6409004483f603d0cc03615344ef38006
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-04 15:58:48 -0230

    Update request_text.py

--- request_text.py
+++ request_text.py
@@ -3,7 +3,7 @@ import OPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.IN)
-api_url = 'http://8.16.250.212:4000/api/printer'
+api_url = 'http://8.16.250.212:4000/api/job'
 myheaders = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'}
 i = 2
 n = 1
@@ -13,3 +13,4 @@ while i > n:
     if GPIO.input(12) == GPIO.HIGH:
         response = requests.post(api_url, json={'command':'cancel'}, headers = myheaders)
         print(response.status_code)
+    
