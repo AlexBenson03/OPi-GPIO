da801940027f883bded9d468f55181db67753e1d
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-06 09:00:23 -0230

    Update request_text.py

--- request_text.py
+++ request_text.py
@@ -5,6 +5,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.IN)
 api_url = 'http://8.16.250.212:4000/api/job'
 myheaders = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'}
+myheaders2 = {'X-api-key': '516586FBB1084996BF54C339BD06A812', 'Content-Type': 'application/json'}
 i = 2
 n = 1
 while i > n:
@@ -15,8 +16,7 @@ while i > n:
         time.sleep(0.5)
         response2 = requests.post('http://8.16.250.212:4001/api/job', json={'command':'cancel'}, headers = myheaders)
         time.sleep(0.5)
-        response3 = requests.post('http://8.16.250.212:4002/api/job', json={'command':'cancel'}, headers = myheaders)
+        response3 = requests.post('http://8.16.250.212:4002/api/job', json={'command':'cancel'}, headers = myheaders2)
         print(response.status_code)
         print(response2.status_code)
         print(response3.status_code)
-    
