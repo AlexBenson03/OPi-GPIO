79acaea1993aaa368b0f03e96b23b82244416cdb
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-06 10:27:14 -0230

    Update request_text.py

--- request_text.py
+++ request_text.py
@@ -1,7 +1,6 @@
-import requests
 import OPi.GPIO as GPIO
 import time
-import headers_lib as h_l
+import stop
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.IN)
 i = 2
@@ -9,10 +8,4 @@ n = 1
 while i > n:
     while GPIO.input(12) == GPIO.LOW:
         time.sleep(0.01)
-    response = requests.post('http://8.16.250.212:4000/api/job', json=h_l.Estop, headers = h_l.myheaders)
-    response2 = requests.post('http://8.16.250.212:4001/api/job', json=h_l.Estop, headers = h_l.myheaders)
-    response3 = requests.post('http://8.16.250.212:4002/api/job', json=h_l.Estop, headers = h_l.myheaders2)
-    print(response.status_code)
-    print(response2.status_code)
-    print(response3.status_code)
-    
+    stop.stopcommd
