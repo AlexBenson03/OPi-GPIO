a72bfb070bc133a4a514ec1d44596667c1755e04
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-06 10:10:50 -0230

    Update request_text.py

--- request_text.py
+++ request_text.py
@@ -1,19 +1,17 @@
 import requests
 import OPi.GPIO as GPIO
 import time
+import headers_lib as h_l
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.IN)
-api_url = 'http://8.16.250.212:4000/api/job'
-myheaders = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'}
-myheaders2 = {'X-api-key': '516586FBB1084996BF54C339BD06A812', 'Content-Type': 'application/json'}
 i = 2
 n = 1
 while i > n:
     while GPIO.input(12) == GPIO.LOW:
         time.sleep(0.01)
-    response = requests.post('http://8.16.250.212:4000/api/job', json={'command':'cancel'}, headers = myheaders)
-    response2 = requests.post('http://8.16.250.212:4001/api/job', json={'command':'cancel'}, headers = myheaders)
-    response3 = requests.post('http://8.16.250.212:4002/api/job', json={'command':'cancel'}, headers = myheaders2)
+    response = requests.post('http://8.16.250.212:4000/api/job', json=h_l.Estop, headers = h_l.myheaders)
+    response2 = requests.post('http://8.16.250.212:4001/api/job', json=h_l.Estop, headers = h_l.myheaders)
+    response3 = requests.post('http://8.16.250.212:4002/api/job', json=h_l.Estop, headers = h_l.myheaders2)
     print(response.status_code)
     print(response2.status_code)
     print(response3.status_code)
