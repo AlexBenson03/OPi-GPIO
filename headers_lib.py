af7433428bfebdd5809183aab9c875d5cce14e28
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-06 14:15:04 -0230

    Delete headers_lib.py

--- headers_lib.py
+++
@@ -1,8 +0,0 @@
-def myheaders0():
-    {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'}
-
-def myheaders1():
-    {'X-api-key': '516586FBB1084996BF54C339BD06A812', 'Content-Type': 'application/json'}
-
-def Estop():
-    {'command': 'pause', 'action':'toggle'}
