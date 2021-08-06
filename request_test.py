ff5cd9f1369fa0eb381e2eb6dfb71bf7d95b3c84
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-06 11:44:51 -0230

    Update request_test.py

--- request_test.py
+++ request_test.py
@@ -1,12 +1,14 @@
 import OPi.GPIO as GPIO
 import time
 import commands
+import requests
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.IN)
 
-while GPIO.input(12) == GPIO.LOW:
-    time.sleep(0.01)
-if GPIO.input(12)==GPIO.HIGH:
-    commands.stopcommand()
+while 2>1:
+    while GPIO.input(12) == GPIO.LOW:
+        time.sleep(0.01)
+    if GPIO.input(12)==GPIO.HIGH:
+        commands.stopcommand()
     
