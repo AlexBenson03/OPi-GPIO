80116d84de125f3657233be03280f41c85516486
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-06 11:31:38 -0230

    Update request_text.py

--- request_text.py
+++ request_text.py
@@ -1,12 +1,12 @@
 import OPi.GPIO as GPIO
 import time
-import stop
+import commands
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.IN)
-i = 2
-n = 1
-while i > n:
-    while GPIO.input(12) == GPIO.LOW:
-        time.sleep(0.01)
-    print("button pressed")
-    stop.stopcommd
+
+while GPIO.input(12) == GPIO.LOW:
+    time.sleep(0.01)
+if GPIO.input(12)==GPIO.HIGH:
+    commands.stopcommand()
+    
