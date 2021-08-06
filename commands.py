03a9c6c2e0b107d31d6b4f1b5bfc3f38ecd9ede0
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-06 11:54:27 -0230

    Update commands.py

--- commands.py
+++ commands.py
@@ -1,6 +1,5 @@
-import requests
-
 def stopcommd():
+    import requests
     r0 = requests.post('http://8.16.250.212:4000/api/job', json={'command': 'cancel'}, headers = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
     r1 = requests.post('http://8.16.250.212:4001/api/job', json={'command': 'cancel'}, headers = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
     r2 = requests.post('http://8.16.250.212:4002/api/job', json={'command': 'cancel'}, headers = {'X-api-key': '516586FBB1084996BF54C339BD06A812', 'Content-Type': 'application/json'})
