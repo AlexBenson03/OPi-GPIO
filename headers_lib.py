171140512def65d99a9cd250c68e4a66e69aab0d
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-06 10:11:22 -0230

    Create headers_lib.py

---
+++ headers_lib.py
@@ -0,0 +1,8 @@
+def myheaders():
+    printerheaders0 = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'}
+
+def myheaders2():
+    printerheaders1 = {'X-api-key': '516586FBB1084996BF54C339BD06A812', 'Content-Type': 'application/json'}
+
+def Estop():
+    {'command': 'pause', 'action':'toggle'}
