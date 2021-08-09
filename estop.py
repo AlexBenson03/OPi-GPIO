5c96702f63baa6ef1868a4403870f636b8c49307
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-09 09:06:42 -0230

    Create estop.py

---
+++ estop.py
@@ -0,0 +1,12 @@
+import requests
+import OPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.Board)
+GPIO.setup(12, GPIO.IN)
+
+while GPIO.input(12) == GPIO.LOW:
+    time.sleep(0.01)
+if GPIO.input(12) == GPIO.HIGH:
+    requests.post('8.16.250.212:4000/api/job', json={'command': 'M112'},headers= {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
+
