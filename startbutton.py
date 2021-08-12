0c7475691ce8b1706c17b313684c8ea61d74a9cd
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-12 15:58:53 -0230

    Create startbutton.py

---
+++ startbutton.py
@@ -0,0 +1,11 @@
+import OPi.GPIO as GPIO
+import time
+import requests
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.IN)
+
+while GPIO.input(12) == GPIO.LOW:
+    time.sleep(0.01)
+if GPIO.input(12) == GPIO.HIGH:
+    requests.post ('http://8.16.250.212:4000/api/job', json = {'command', 'start'}, headers = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
