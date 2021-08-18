693268e53475a576594cc2b0486126ea3a249918
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-18 15:57:17 -0230

    Update Flaskserver.py

--- Flaskserver.py
+++ Flaskserver.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
 import json
-import flaskserverbutton
+import time
+import OPi.GPIO as GPIO
+import requests
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.IN)
 
 app = Flask(__name__)
 
@@ -14,8 +19,12 @@ def Posting():
     extra_data = json.loads(form_data['extra'])
     print(form_data['topic'])
     print(extra_data['name'])
+    name_data = extra_data['name']
     while form_data['topic'] == 'Print Done':
-        flaskserverbutton.restartbutton
+        while GPIO.input(12) == GPIO.LOW:
+            time.sleep(0.01)
+        if GPIO.input(12) == GPIO.HIGH:
+            requests.post ('http://8.16.250.212:4000/api/files/'(name_data), json = {'command': 'select', 'print': 'true'}, headers = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
 
     return 'JSON posted'
 
