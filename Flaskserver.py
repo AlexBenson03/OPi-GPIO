b20a93e1be6c726c2fb5cae45c569c3bcafe3f9b
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-19 09:45:55 -0230

    Update Flaskserver.py

--- Flaskserver.py
+++ Flaskserver.py
@@ -18,15 +18,17 @@ def Posting():
     form_data = request.form.to_dict()
     extra_data = json.loads(form_data['extra'])
     name_data = extra_data['name']
+    url_var = f'http://8.16.250.212:4000/api/files/{name_data}'
+    print(url_var)
     print(form_data['topic'])
     print(extra_data['name'])
-    while form_data['topic'] == 'Print Done':
-        while GPIO.input(12) == GPIO.LOW:
-            time.sleep(0.01)
-            print("waiting for button")
-        if GPIO.input(12) == GPIO.HIGH:
-            requests.post (f'http://8.16.250.212:4000/api/files/{name_data}', json = {'command': 'select', 'print': 'true'}, headers = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
-            print("button pressed")
+    # while form_data['topic'] == 'Print Done':
+    #     while GPIO.input(12) == GPIO.LOW:
+    #         time.sleep(0.01)
+    #         print("waiting for button")
+    #     if GPIO.input(12) == GPIO.HIGH:
+    #         requests.post (f'http://8.16.250.212:4000/api/files/{name_data}', json = {'command': 'select', 'print': 'true'}, headers = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
+    #         print("button pressed")
     return 'JSON posted'
 
 
