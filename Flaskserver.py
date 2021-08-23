22c9daccb3f2e5b6d36c2a6b5158f0e12936acf9
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-23 09:44:52 -0230

    Update Flaskserver.py

--- Flaskserver.py
+++ Flaskserver.py
@@ -4,6 +4,7 @@ import time
 import OPi.GPIO as GPIO
 import requests
 
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.IN)
 
@@ -13,23 +14,25 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 @app.route('/Posting', methods=['POST'])
 def Posting():
-    printjob = open("printjob.txt", "a")
-    form_data = request.form.to_dict()
-    extra_data = json.loads(form_data['extra'])
-    name_data = extra_data['name']
-    printjob.write(name_data)
-    printjob.close()
-    printjob = open("printjob.txt", "r")
-    print(printjob.read())
-    while form_data['topic'] == 'Print Done':
-        while GPIO.input(12) == GPIO.LOW:
-            time.sleep(0.01)
-        if GPIO.input(12) == GPIO.HIGH:
-            requests.post (f'http://8.16.250.212:4000/api/files/local/{name_data}', json = {'command': 'select', 'print': 'true'}, headers = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
-        break
-    return 'JSON posted'
+   form_data = request.form.to_dict()
+   extra_data = json.loads(form_data['extra'])
+   name_data = extra_data['name']
+   print_name = [extra_data['name']]
+   with open("printjob.txt", "w") as printjob:
+        printjob.writelines("%s\n" % line for line in print_name)
+   printjob.close()
+   printjob = open("printjob.txt", "r")
+   print(printjob.read())
+   while form_data['topic'] == 'Print Done':
+       while GPIO.input(12) == GPIO.LOW:
+           time.sleep(0.01)
+       if GPIO.input(12) == GPIO.HIGH:
+          requests.post (f'http://8.16.250.212:4000/api/files/local/{name_data}', json = {'command': 'select', 'print': 'true'}, headers = {'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
+       break
+   return 'JSON posted'
 
 
 if __name__ =='__main__':
