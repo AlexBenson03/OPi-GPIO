5d7cc51aae38aa43d052f561ecfef205b2dc9052
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-24 13:22:26 -0230

    Update Flaskserver.py

--- Flaskserver.py
+++ Flaskserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+rom flask import Flask, request
 import json
 import time
 import OPi.GPIO as GPIO
@@ -24,14 +24,16 @@ def Posting():
     form_data = request.form.to_dict()
     extra_data = json.loads(form_data['extra'])
     #name_data = extra_data['name']
-    with open("printjob.txt", "w") as print_job:
+    with open('printjob.txt', 'w') as print_job:
         print_job.writelines("%s\n" % line for line in extra_data['name'])
         print_job.close()
-    read_job = open("printjob.txt", "r")
-    #while form_data['topic'] == 'Print Done':
+    read_job = open('printjob.txt', 'r')
     while GPIO.input(12) == GPIO.LOW:
             time.sleep(0.01)
             if GPIO.input(12) == GPIO.HIGH:
+                req_get = requests.get('http://8.16.250.212:4000//api/job', headers={
+                        'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
+                print(json.dumps(req_get))
                 #print(form_data['topic'])
                 print(read_job)
                 requests.post(f'http://8.16.250.212:4000/api/files/local/{read_job}', json={'command': 'select'}, headers={
