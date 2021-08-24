a5cfaa5c69a62683f013c8675e401b07c93a369b
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-24 11:01:39 -0230

    Update Flaskserver.py

--- Flaskserver.py
+++ Flaskserver.py
@@ -24,15 +24,17 @@ def Posting():
     form_data = request.form.to_dict()
     extra_data = json.loads(form_data['extra'])
     name_data = extra_data['name']
-    print(form_data['topic'])
-    print(extra_data['name'])
+    with open("printjob.txt", "w") as print_job:
+        print_job.writelines("%s\n" % line for line in name_data)
+        print_job.close()
+    read_job = open("printjob.txt", "r")
     while form_data['topic'] == 'Print Done':
         while GPIO.input(12) == GPIO.LOW:
             time.sleep(0.01)
         if GPIO.input(12) == GPIO.HIGH:
             print(form_data['topic'])
             print(extra_data['name'])
-            requests.post(f'http://8.16.250.212:4000/api/files/local/{name_data}', json={'command': 'select'}, headers={
+            requests.post(f'http://8.16.250.212:4000/api/files/local/{read_job}', json={'command': 'select'}, headers={
                         'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
             time.sleep(0.5)
             requests.post('http://8.16.250.212:4000/api/job', json={'command': 'start'}, headers={
@@ -42,4 +44,3 @@ def Posting():
 
 if __name__ =='__main__':
     app.run(debug=True, host='0.0.0.0')
-
