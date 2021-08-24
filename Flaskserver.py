1099d0c97cfd0bcce3d2ddbdee0f252e5359c0da
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-24 16:05:01 -0230

    Update Flaskserver.py

--- Flaskserver.py
+++ Flaskserver.py
@@ -18,31 +18,14 @@ def hello_world():
 
 @app.route('/Posting', methods=['POST', 'GET'])
 def Posting():
-    print("-----------")
-    print(json.dumps(request.form))
-    print("-----------")
-    # form_data = request.form.to_dict()
-    # extra_data = json.loads(form_data['extra'])
-    # #name_data = extra_data['name']
-
-
-    # with open('printjob.txt', 'w') as print_job:
-    #     print_job.writelines("%s\n" % line for line in extra_data['name'])
-    #     print_job.close()
-    # read_job = open('printjob.txt', 'r')
-
-
     while GPIO.input(12) == GPIO.LOW:
             time.sleep(0.01)
             if GPIO.input(12) == GPIO.HIGH:
                 req_data = requests.get('http://8.16.250.212:4000//api/job', headers={
                         'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
                 name_data = json.loads(req_data.text)
-                print(name_data)
                 job_data = name_data['job']
-                print(job_data)
                 file_data = job_data['file']
-                print(file_data)
                 print_job = file_data['name']
                 requests.post(f'http://8.16.250.212:4000/api/files/local/{print_job}', json={'command': 'select'}, headers={
                          'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
@@ -54,4 +37,3 @@ def Posting():
 
 if __name__ =='__main__':
     app.run(debug=True, host='0.0.0.0')
-
