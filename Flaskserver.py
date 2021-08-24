77e286dd30d2f98bcfaa138676481de183867937
AlexBenson03 <86978576+AlexBenson03@users.noreply.github.com>
2021-08-24 11:34:07 -0230

    Update Flaskserver.py

--- Flaskserver.py
+++ Flaskserver.py
@@ -23,21 +23,21 @@ def Posting():
     print("-----------")
     form_data = request.form.to_dict()
     extra_data = json.loads(form_data['extra'])
-    name_data = extra_data['name']
+    #name_data = extra_data['name']
     with open("printjob.txt", "w") as print_job:
-        print_job.writelines("%s\n" % line for line in name_data)
+        print_job.writelines("%s\n" % line for line in extra_data['name'])
         print_job.close()
     read_job = open("printjob.txt", "r")
-    while form_data['topic'] == 'Print Done':
-        while GPIO.input(12) == GPIO.LOW:
+    #while form_data['topic'] == 'Print Done':
+    while GPIO.input(12) == GPIO.LOW:
             time.sleep(0.01)
-        if GPIO.input(12) == GPIO.HIGH:
-            print(form_data['topic'])
-            print(extra_data['name'])
-            requests.post(f'http://8.16.250.212:4000/api/files/local/{read_job}', json={'command': 'select'}, headers={
+            if GPIO.input(12) == GPIO.HIGH:
+                print(form_data['topic'])
+                print(read_job)
+                requests.post(f'http://8.16.250.212:4000/api/files/local/{read_job}', json={'command': 'select'}, headers={
                         'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
-            time.sleep(0.5)
-            requests.post('http://8.16.250.212:4000/api/job', json={'command': 'start'}, headers={
+                time.sleep(0.5)
+                requests.post('http://8.16.250.212:4000/api/job', json={'command': 'start'}, headers={
                         'X-api-key': '0FF9258103494737B416217A10687F1B', 'Content-Type': 'application/json'})
     return 'JSON posted'
 
